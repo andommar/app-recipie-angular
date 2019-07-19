2da2f34b90dd2dbaa7f029d5930e753ff4785044
andommar <43680127+andommar@users.noreply.github.com>
2019-07-19 13:20:51 +0200

    Add files via upload

---
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipie } from '../recipie.model';
+
+@Component({
+  selector: 'app-recipie-list',
+  templateUrl: './recipie-list.component.html',
+  styleUrls: ['./recipie-list.component.css']
+})
+export class RecipieListComponent implements OnInit {
+
+  recipies: Recipie[] = [
+    new Recipie ('Macarrones', 'Están riuquisimos',
+    'https://www.cocinacaserayfacil.net/wp-content/uploads/2018/07/macarrones-con-tomate-y-atun.jpg'),
+    new Recipie ('Pollo al chilindrón', 'Qué rico está mañico!',
+    'http://recetasdecocina.elmundo.es/wp-content/uploads/2016/10/receta-pollo-al-chilindron.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
